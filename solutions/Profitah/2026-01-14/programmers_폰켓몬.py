fc70f6793884b1e763906251f2146312a338b74c
이윤아 <qwer120542@gmail.com>
2026-01-28 12:29:03 +0900

    잘못 수정한거 복원

--- solutions/Profitah/2026-01-14/programmers_폰켓몬.py
+++ solutions/Profitah/2026-01-14/programmers_폰켓몬.py
@@ -1,19 +1,7 @@
-"""
-출력해야할 것은 : 완주하지 못한 선수의 이름.
-["leo", "kiki", "eden"]	["eden", "kiki"] 가 주어졌을때
-완주하지 못한 사람은 completion 배열에 없는 "leo"
-"""
-from collections import Counter # 딕셔너리 형태로 저장, 이름개수 세기 위해 필요
+def solution(nums):
+    solution = set(nums)  # nums 배열을 set으로 변환 (중복 제거)
+    return min(len(solution), len(nums) // 2)  
+    """ set의 크기(len(solution)) = 폰켓몬 종류 수 와
+     배열 길이 // 2 = 선택할 수 있는 최대 마리 수 중
+     더 작은 값을 반환 """
 
-def solution(participant, completion):
-    # 참가자 명단의 이름 개수를 세서 딕셔너리 형태로 저장
-    p_count = Counter(participant)
-    
-    # 완주자 명단의 이름 개수를 세서 저장
-    c_count = Counter(completion)
-    
-    # 참가자 - 완주자 = 완주하지 못한 사람만 남음
-    answer = p_count - c_count
-    
-    # 딕셔너리에 남아 있는 유일한 키(완주 못한 선수 이름) 반환
-    return next(iter(answer))
