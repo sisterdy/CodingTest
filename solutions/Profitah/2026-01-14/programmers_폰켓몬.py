a1dd33b63f39bc10440e52ba7bab1ed1dbe855d5
이윤아 <qwer120542@gmail.com>
2026-02-04 20:01:46 +0900

    Profitah: 2026-01-28 문제 풀이

--- solutions/Profitah/2026-01-14/programmers_폰켓몬.py
+++ solutions/Profitah/2026-01-14/programmers_폰켓몬.py
@@ -5,3 +5,9 @@ def solution(nums):
      배열 길이 // 2 = 선택할 수 있는 최대 마리 수 중
      더 작은 값을 반환 """
 
+def solution(nums):
+    solution = set(nums)  # nums 배열을 set으로 변환 (중복 제거)
+    return min(len(solution), len(nums) // 2)  
+    """ set의 크기(len(solution)) = 폰켓몬 종류 수 와
+     배열 길이 // 2 = 선택할 수 있는 최대 마리 수 중
+     더 작은 값을 반환 """
